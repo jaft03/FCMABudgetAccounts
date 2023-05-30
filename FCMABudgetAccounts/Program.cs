21a2cd9517b07e8df565465c52414e317a0ae23a
Lee Stallard <lee.e.stallard@gmail.com>
2023-05-30 15:32:56 -0400

    Configured key vault access for Production deployment.

--- FCMABudgetAccounts/Program.cs
+++ FCMABudgetAccounts/Program.cs
@@ -2,9 +2,20 @@ using System;
 using System.Collections.Generic;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Storage;
+using Azure.Identity;
+using Azure.Security.KeyVault.Secrets;
 using FCMABudgetAccounts.Database;
 
+// set up web application builder
 var builder = WebApplication.CreateBuilder(args);
+
+// configure key vault
+if (builder.Environment.IsProduction())
+{
+    builder.Configuration.AddAzureKeyVault(
+        new Uri($"https://{builder.Configuration["KeyVaultName"]}.vault.azure.net/"),
+        new DefaultAzureCredential());
+}
 
 // Add services to the container.
 builder.Services.AddControllers();
@@ -12,6 +23,16 @@ builder.Services.AddControllers();
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
+
+// configure swagger
+builder.Services.ConfigureSwaggerGen(setup =>
+{
+    setup.SwaggerDoc("v1", new Microsoft.OpenApi.Models.OpenApiInfo
+    {
+        Title = "FCMA Budget Accounts",
+        Version = "v1"
+    });
+});
 
 // set up database access
 builder.Services.AddDbContext<FcmaBudgetsDbContext>(
